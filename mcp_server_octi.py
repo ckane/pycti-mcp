cd836effa4e5e46ba233f654602622d6eeb1161f
Coleman Kane <ckane@colemankane.org>
2025-06-14 19:46:11 -0400

    Support choosing SSE or STDIO servers

--- mcp_server_octi.py
+++ mcp_server_octi.py
@@ -9,7 +9,8 @@ from mcp.server.fastmcp import FastMCP
 def main():
     logging.basicConfig(level='INFO')
     ap = ArgumentParser(description="Execute the OpenCTI MCP Server")
-    ap.add_argument('-p', '--port', required=False, type=int, default=8002, help='TCP port to listen on (default 8002)')
+    ap.add_argument('-p', '--port', required=False, type=int, default=8002, help='TCP port to listen on (default 8002 - ignored if -s)')
+    ap.add_argument('-s', '--stdio', required=False, default=False, action="store_true", help='Start an STDIO server (default: off)')
     args = ap.parse_args()
     mcp = FastMCP("OpenCTI.MCP", port=args.port)
 
@@ -23,7 +24,10 @@ def main():
             print(f'Failed to load ToolSpec from pycti_tools.{m}')
             raise e
 
-    asyncio.run(mcp.run_sse_async())
+    if args.stdio:
+        asyncio.run(mcp.run_stdio_async())
+    else:
+        asyncio.run(mcp.run_sse_async())
 
 if __name__ == "__main__":
     main()
