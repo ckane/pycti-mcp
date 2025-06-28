39e9b6c38e8d23f4341834b914dbeeadf3ef2933
Coleman Kane <ckane@colemankane.org>
2025-06-27 20:19:53 -0400

    Clean up code w/ a formatter

--- mcp_server_octi.py
+++ mcp_server_octi.py
@@ -6,28 +6,46 @@ import logging
 from argparse import ArgumentParser
 from mcp.server.fastmcp import FastMCP
 
+
 def main():
-    logging.basicConfig(level='INFO')
+    logging.basicConfig(level="INFO")
     ap = ArgumentParser(description="Execute the OpenCTI MCP Server")
-    ap.add_argument('-p', '--port', required=False, type=int, default=8002, help='TCP port to listen on (default 8002 - ignored if -s)')
-    ap.add_argument('-s', '--stdio', required=False, default=False, action="store_true", help='Start an STDIO server (default: off)')
+    ap.add_argument(
+        "-p",
+        "--port",
+        required=False,
+        type=int,
+        default=8002,
+        help="TCP port to listen on (default 8002 - ignored if -s)",
+    )
+    ap.add_argument(
+        "-s",
+        "--stdio",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Start an STDIO server (default: off)",
+    )
     args = ap.parse_args()
     mcp = FastMCP("OpenCTI.MCP", port=args.port)
 
     # Dynamically walk through ./pycti_tools/ and import each tool into MCP via its ToolSpec
     for m in pycti_tools.__all__:
-        tmpmod = importlib.import_module(f'pycti_tools.{m}')
+        tmpmod = importlib.import_module(f"pycti_tools.{m}")
         try:
-            mcp.add_tool(tmpmod.ToolSpec.fn, tmpmod.ToolSpec.name, tmpmod.ToolSpec.description)
-            print(f'Added {tmpmod.ToolSpec} to MCP')
-        except e:
-            print(f'Failed to load ToolSpec from pycti_tools.{m}')
+            mcp.add_tool(
+                tmpmod.ToolSpec.fn, tmpmod.ToolSpec.name, tmpmod.ToolSpec.description
+            )
+            print(f"Added {tmpmod.ToolSpec} to MCP")
+        except Exception as e:
+            print(f"Failed to load ToolSpec from pycti_tools.{m}")
             raise e
 
     if args.stdio:
         asyncio.run(mcp.run_stdio_async())
     else:
         asyncio.run(mcp.run_sse_async())
+
 
 if __name__ == "__main__":
     main()
--- pycti_tools/lookup_adversary.py
+++ pycti_tools/lookup_adversary.py
@@ -6,51 +6,63 @@ from typing import Annotated
 from pycti import OpenCTIApiClient
 from .local_settings import api_url, api_token
 
+
 # Parse a "Threat Adversary" when fetched from the system
 def parse_adv(ta):
     parsed_ta = {
-        'stix_id': ta['standard_id'],
-        'opencti_id': ta['id'],
-        'name': ta['name'],
-        'data_type': ta['entity_type'],
-        'description': ta['description'],
-        'created': ta['created_at'],
-        'last_updated': ta['updated_at'],
-        'labels': [l['value'] for l in ta['objectLabel']],
-        'first_seen': ta['first_seen'],
-        'last_seen': ta['last_seen'],
-        'external_reports': [
-            {'name': r['name'], 'urls': [e['url'] for e in r['externalReferences']]} for r in ta['reports']
-        ] + [
-            {'name': 'Self', 'urls': [e['url'] for e in ta['externalReferences']]}
+        "stix_id": ta["standard_id"],
+        "opencti_id": ta["id"],
+        "name": ta["name"],
+        "data_type": ta["entity_type"],
+        "description": ta["description"],
+        "created": ta["created_at"],
+        "last_updated": ta["updated_at"],
+        "labels": [l["value"] for l in ta["objectLabel"]],
+        "first_seen": ta["first_seen"],
+        "last_seen": ta["last_seen"],
+        "external_reports": [
+            {"name": r["name"], "urls": [e["url"] for e in r["externalReferences"]]}
+            for r in ta["reports"]
+        ]
+        + [{"name": "Self", "urls": [e["url"] for e in ta["externalReferences"]]}],
+        "notes": [note["content"] for note in ta["notes"]],
+        "opinions": [
+            {"sentiment": op["opinion"], "explanation": op["explanation"]}
+            for op in ta["opinions"]
         ],
-        'notes': [note['content'] for note in ta['notes']],
-        'opinions': [{'sentiment': op['opinion'], 'explanation': op['explanation']} for op in ta['opinions']],
     }
 
     # Safely populate optional keys if they exist in the source object
     for optkey in [
-            'aliases', 'goals', 'roles', 'sophistication', 'primary_motivation', 'secondary_motivations', 'objective', 'resource_level'
+        "aliases",
+        "goals",
+        "roles",
+        "sophistication",
+        "primary_motivation",
+        "secondary_motivations",
+        "objective",
+        "resource_level",
     ]:
         if optkey in ta:
             parsed_ta[optkey] = ta[optkey]
 
     return parsed_ta
 
-notes_projection="""
+
+notes_projection = """
     id
     content
     authors
 """
 
-opinions_projection="""
+opinions_projection = """
     id
     opinion
     explanation
     authors
 """
 
-reports_projection="""
+reports_projection = """
     id
     standard_id
     name
@@ -75,7 +87,7 @@ reports_projection="""
     }
 """
 
-ta_projection="""
+ta_projection = """
     id
     standard_id
     name
@@ -127,6 +139,7 @@ ta_projection="""
     }
 """
 
+
 # Should look up campaign, intrusion_set, threat_actor_group, and threat_actor_individual
 def opencti_adversary_lookup(
     name: Annotated[str, "The adversary or threat name or alias to look up in OpenCTI"],
@@ -135,22 +148,33 @@ def opencti_adversary_lookup(
     | None
 ):
     """Given a name or alias of a threat adversary, look it up in OpenCTI. If it is stored in OpenCTI return a JSON
-       data structure with information about it. Can be used to look up Threat Actors, Threat Actor Groups, Campaigns, Individuals,
-       and Intrusion Sets. If it isn't found, None will be returned."""
+    data structure with information about it. Can be used to look up Threat Actors, Threat Actor Groups, Campaigns, Individuals,
+    and Intrusion Sets. If it isn't found, None will be returned."""
     log = logging.getLogger(name="octimcp")
     octi = OpenCTIApiClient(url=api_url, token=api_token, ssl_verify=True)
 
-    adversary_types = [octi.campaign, octi.intrusion_set, octi.threat_actor_group, octi.threat_actor_individual]
+    adversary_types = [
+        octi.campaign,
+        octi.intrusion_set,
+        octi.threat_actor_group,
+        octi.threat_actor_individual,
+    ]
 
     ta_list = []
 
     for adv_type in adversary_types:
         try:
-            ta = adv_type.read(filters={
-                "mode": "or",
-                "filters": [{"key": "name", "values": [name]}, {"key": "aliases", "values": [name]}],
-                "filterGroups": [],
-            }, customAttributes=ta_projection)
+            ta = adv_type.read(
+                filters={
+                    "mode": "or",
+                    "filters": [
+                        {"key": "name", "values": [name]},
+                        {"key": "aliases", "values": [name]},
+                    ],
+                    "filterGroups": [],
+                },
+                customAttributes=ta_projection,
+            )
             log.debug(f"Got {json.dumps(ta)}")
 
             if ta == None:
@@ -216,6 +240,7 @@ def opencti_adversary_lookup(
             raise e
 
     return ta_list if ta_list else None
+
 
 class ToolSpec:
     name = "opencti_adversary_lookup"
