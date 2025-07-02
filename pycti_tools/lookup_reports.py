044bc08f8d2fdc1b63818b96610b83bbdfe11513
Coleman Kane <ckane@colemankane.org>
2025-07-02 01:13:19 -0400

    Reformat the lookup_reports code and fix a few bugs in it

--- pycti_tools/lookup_reports.py
+++ pycti_tools/lookup_reports.py
@@ -7,48 +7,52 @@ from typing import Annotated
 from pycti import OpenCTIApiClient
 from .local_settings import api_url, api_token
 
-desired_obj_fields = ['value', 'name', 'pattern', 'pattern_type', 'observable_value']
+desired_obj_fields = ["value", "name", "pattern", "pattern_type", "observable_value"]
+
 
 def filter_object(o):
-  if 'relationship_type' in o:
+    if "relationship_type" in o:
+        return False
+
+    for f in desired_obj_fields:
+        if f in o:
+            return True
+
     return False
 
-  for f in desired_obj_fields:
-    if f in o:
-      return True
-
-  return False
 
 def translate_object(o):
-  new_o = {
-    'entity_type': o['entity_type'],
-    'opencti_id': o['id'],
-    'stix_id': o['standard_id'],
-  }
+    new_o = {
+        "entity_type": o["entity_type"],
+        "opencti_id": o["id"],
+        "stix_id": o["standard_id"],
+    }
 
-  for f in desired_obj_fields:
-    if f in o:
-      new_o[f] = o[f]
+    for f in desired_obj_fields:
+        if f in o:
+            new_o[f] = o[f]
 
-  return new_o
+    return new_o
+
 
 def parse_rpt(rpt: dict) -> dict:
-  parsed_rpt = {
-    'stix_id': rpt['standard_id'],
-    'opencti_id': rpt['id'],
-    'labels': [l['value'] for l in rpt['objectLabel']],
-    'data_type': rpt['entity_type'],
-    'description': rpt['description'],
-    'name': rpt['name'],
-    'created': rpt['created'],
-    'modified': rpt['modified'],
-    'published': rpt['published'],
-    'report_types': rpt['report_types'],
-    'external_urls': [e['url'] for e in rpt['externalReferences']],
-    'objects': [translate_object(o) for o in filter(filter_object, rpt['objects'])],
-  }
+    parsed_rpt = {
+        "stix_id": rpt["standard_id"],
+        "opencti_id": rpt["id"],
+        "labels": [l["value"] for l in rpt["objectLabel"]],
+        "data_type": rpt["entity_type"],
+        "description": rpt["description"],
+        "name": rpt["name"],
+        "created": rpt["created"],
+        "modified": rpt["modified"],
+        "published": rpt["published"],
+        "report_types": rpt["report_types"],
+        "external_urls": [e["url"] for e in rpt["externalReferences"]],
+        "objects": [translate_object(o) for o in filter(filter_object, rpt["objects"])],
+    }
 
-  return parsed_rpt
+    return parsed_rpt
+
 
 report_projection = """
 id
@@ -192,6 +196,7 @@ objects(all: true) {
 }
 """
 
+
 # Look up any reports in the system that match the criteria
 # TODO: Look across reports, cases, malware analyses, and groupings
 def opencti_reports_lookup(
@@ -200,55 +205,73 @@ def opencti_reports_lookup(
     search: Annotated[str | None, "Search terms to filter"] = None,
 ) -> Annotated[list | None, "Data structure listing the discovered reports"]:
     """Given a date range (start and end date) and some search terms, find all reports in the system
-       matching the given criteria"""
+    matching the given criteria"""
     log = logging.getLogger(name="octimcp")
     octi = OpenCTIApiClient(url=api_url, token=api_token, ssl_verify=True)
 
-    log.info(f'Searching for reports between {earliest} and {latest} via search term "{search}"')
-
-    try:
-        fargs = {
-          'orderMode': 'desc',
-          'orderBy': 'published',
-          'customAttributes': report_projection,
-        }
-
-        if search:
-            fargs['search'] = search
-
-        # If earliest or latest are provided, then build a filter for them
-        if earliest or latest:
-          daterange_filter = {
-            'mode': 'and',
-            'filters': [],
-            'filterGroups': [],
-          }
-
-          if earliest:
-            earliest_dt = dateparse(earliest)
-            daterange_filter['filters'].append({'key': 'published', 'values': [earliest_dt.isoformat()], 'operator': 'gte', 'mode': 'and'})
-
-          if latest:
-            latest_dt = dateparse(latest)
-            daterange_filter['filters'].append({'key': 'published', 'values': [latest_dt.isoformat()], 'operator': 'lte', 'mode': 'and'})
-
-          fargs['filters'] = daterange_filter
-
-        log.debug(f'Query: {fargs}')
-        r = octi.report.list(**fargs)
-    except e:
-        log.error(f'There was an error {e}')
+    log.info(
+        f'Searching for reports between {earliest} and {latest} via search term "{search}"'
+    )
 
     rpts_list = []
 
-    log.debug(f'{len(r)} Reports found')
+    try:
+        fargs = {
+            "orderMode": "desc",
+            "orderBy": "published",
+            "customAttributes": report_projection,
+            "filters": {},
+        }
 
-    for rpt in r:
-      parsed_rpt = parse_rpt(rpt)
-      rpts_list.append(parsed_rpt)
-      log.debug(f'Report result: {json.dumps(parsed_rpt)}')
+        if search:
+            fargs["search"] = search
+
+        # If earliest or latest are provided, then build a filter for them
+        if earliest or latest:
+            daterange_filter = {
+                "mode": "and",
+                "filters": [],
+                "filterGroups": [],
+            }
+
+            if earliest:
+                earliest_dt = dateparse(earliest)
+                daterange_filter["filters"].append(
+                    {
+                        "key": "published",
+                        "values": [earliest_dt.isoformat()],
+                        "operator": "gte",
+                        "mode": "and",
+                    }
+                )
+
+            if latest:
+                latest_dt = dateparse(latest)
+                daterange_filter["filters"].append(
+                    {
+                        "key": "published",
+                        "values": [latest_dt.isoformat()],
+                        "operator": "lte",
+                        "mode": "and",
+                    }
+                )
+
+            fargs["filters"] = daterange_filter
+
+        log.debug(f"Query: {fargs}")
+        r = octi.report.list(**fargs)
+
+        log.debug(f"{len(r)} Reports found")
+
+        for rpt in r:
+            parsed_rpt = parse_rpt(rpt)
+            rpts_list.append(parsed_rpt)
+            log.debug(f"Report result: {json.dumps(parsed_rpt)}")
+    except Exception as e:
+        log.error(f"There was an error {e}")
 
     return rpts_list
+
 
 class ToolSpec:
     name = "opencti_reports_lookup"
